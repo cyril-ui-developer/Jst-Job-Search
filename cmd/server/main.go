badb1acf2f7b0de4bd317c512cc543d44e3d0272
Cyril <cyril.ajieh@gmail.com>
2021-04-06 18:59:03 -0400

    initial commit

---
+++ cmd/server/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("JST Job Saerch REST API, implemented in Golang" )
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github/cyril-ui-developer/JstJobSearch
+
+go 1.14
