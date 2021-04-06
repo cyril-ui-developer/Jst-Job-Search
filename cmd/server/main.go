2dd5b530843a73e9b6d3a662c55327a380253f30
Cyril <cyril.ajieh@gmail.com>
2021-04-06 19:45:20 -0400

    add health route

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	transportHTTP "github.com/cyril-ui-developer/Jst-Job-Search/internal/transport/http"
+)
 
-func main(){
-	fmt.Println("JST Job Saerch REST API, implemented in Golang" )
+//App - the struct which contains things like pointers to db connections
+type App struct{}
+
+func (app *App) Run() error {
+	fmt.Println("Setting Our APP")
+
+	handler := transportHTTP.NewHandler()
+	handler.SetupRoutes()
+
+	if err := http.ListenAnd.Serve(":8080", handler.Router); err != nil {
+		fmt.Println("Failed to set server")
+		return err
+	}
+	return nil
+}
+
+func main() {
+	fmt.Println("JST Job Saerch REST API, implemented in Golang")
+
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Error starting up JST Job Saerch REST API")
+		fmt.Println(err)
+	}
 }
---
+++ internal/transport/http/handler.go
@@ -0,0 +1,23 @@
+package http
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+)
+//Handler - stores the pointer to the jobs service
+type Handler struct{
+	Router *mux.Router
+}
+
+func NewHandler() *Handler {
+	return &Handler{}
+}
+
+//SetupRoutes - sets up all the routes for the app
+func (h *Haandler) SetupRoutes(){
+	fmt.Println("Setting Up Routes")
+	h.Router = mux.NewHandler()
+	h.Router.HaandleFunc("/api/health", funct(w http.ResponseWriter, *http.Request){
+		fmt.Println("w, I'm alive!")
+	})
+}
